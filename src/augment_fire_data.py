99ec5fd688075f25828310c154f0893ca977a19e
MatthewSheppard1 <matthewsheppard02@gmail.com>
2026-01-14 11:15:31 -0700

    Adapted augment_fire_data.py to request 7-day data, added fires_week_long.csv

--- src/augment_fire_data.py
+++ src/augment_fire_data.py
@@ -1,17 +1,29 @@
 from meteostat import Point, Hourly
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 
+# for each fire date, lat and long
+#   add fire_index to be able to associate each entry to the same fire
+#   add day [1-7]
+#   request data for date and time then loop through subtracting 1 from day (plan for potential month change or disregard)
+#   request the same series of time 1 year earlier labeled as no fire
+
+
+data_range = range(0, 1000)
 base_dir = os.getcwd()
 
 fire_data = pd.read_csv(base_dir + '\\..\\data\\fires.csv')
 
-count = 0
+count = data_range[0]
 
 augmented_data = []
 
 for i, row in fire_data.iterrows():
+    if int(i) > data_range[-1]:
+        break
+    if int(i) not in data_range:
+        continue
     object_id = row['OBJECTID']
 
     date, time = row['attr_FireDiscoveryDateTime'].split(" ")
@@ -29,36 +41,44 @@ for i, row in fire_data.iterrows():
     lat = float(row['attr_InitialLatitude'])
     long = float(row['attr_InitialLongitude'])
 
+    # Retrieve week-long fire data
     fire_date = datetime(year, month, day, hour)
+    fire_week_before_date = fire_date - timedelta(days=6)
     fire_loc = Point(lat, long)
 
-    weather = Hourly(fire_loc, fire_date, fire_date)
+    weather = Hourly(fire_loc, fire_week_before_date, fire_date)
     weather_data = weather.fetch()
-    try:
-        previous_year_date = datetime(year - 1, month, day, hour)
-        weather2 = Hourly(fire_loc, previous_year_date, previous_year_date)
-        weather_data_2 = weather2.fetch()
+    weather_data = pd.DataFrame(weather_data)
 
-        if not weather_data.empty and not weather_data_2.empty:
-            augmented_row = pd.concat([fire_data.iloc[i], weather_data.iloc[0]], ignore_index=True)
-            augmented_row = pd.concat([augmented_row, pd.Series([1])])
-            augmented_data.append(augmented_row.to_numpy())
+    # If data is retrieved, filter for same hour every day of the past week
+    if not weather_data.empty:
+        daily_data = weather_data.loc[weather_data.index.hour == hour]
+        previous_year_end_date = fire_date - timedelta(days=365)
+        previous_year_start_date = fire_week_before_date - timedelta(days=365)
+        previous_year_weather = Hourly(fire_loc, previous_year_start_date, previous_year_end_date)
+        previous_year_weather_data = previous_year_weather.fetch()
 
-            previous_year_row = fire_data.iloc[i].copy()
-            previous_year_row['attr_FireDiscoveryDateTime'] = date[0] + "/" + date[1] + "/" + str(year - 1) + " " + str(time[0]) + ":" + str(time[1])
-            augmented_row_2 = pd.concat([previous_year_row, weather_data_2.iloc[0]], ignore_index=True)
-            augmented_row_2 = pd.concat([augmented_row_2, pd.Series([0])])
-            augmented_data.append(augmented_row_2.to_numpy())
+        # If data for a year earlier is retrieved, filter for one entry per day at the same hour
+        if not previous_year_weather_data.empty:
+            previous_year_daily_data = previous_year_weather_data.loc[previous_year_weather_data.index.hour == hour]
+
+            # make time a normal column rather than the index
+            daily_data = daily_data.reset_index()
+            for index, daily_row in daily_data.iterrows():
+                augmented_row = pd.concat([pd.Series([object_id]), pd.Series([index + 1]), daily_data.iloc[index], pd.Series([1])])
+                augmented_data.append(augmented_row.to_numpy())
+
+            previous_year_daily_data = previous_year_daily_data.reset_index()
+            for index, daily_row in previous_year_daily_data.iterrows():
+                augmented_row = pd.concat([pd.Series([object_id]), pd.Series([index + 1]), previous_year_daily_data.iloc[index], pd.Series([0])])
+                augmented_data.append(augmented_row.to_numpy())
+
             count += 1
             print(count)
-    except ValueError as e:
-        print('Value Error: ', e)
 
 # temp,dwpt,rhum,prcp,snow,wdir,wspd,wpgt,pres,tsun,coco,hasFire
-cols = ['id', 'object_id', 'date_time', 'latitude', 'longitude', 'temperature', 'dewpoint', 'relative_humidity', 'precipitation', 'snow', 'wind_direction', 'wind_speed', 'wind_gust', 'air_pressure', 'sunshine', 'weather_code', 'fire']
+cols = ['object_id', 'day', 'date_time', 'temperature', 'dewpoint', 'relative_humidity', 'precipitation', 'snow', 'wind_direction', 'wind_speed', 'wind_gust', 'air_pressure', 'sunshine', 'weather_code', 'fire']
 
 augmented_data_df = pd.DataFrame(augmented_data, columns=cols)
-augmented_data_df = augmented_data_df.drop('object_id', axis=1)
-augmented_data_df = augmented_data_df.drop('id', axis=1)
 
-augmented_data_df.to_csv(base_dir + '\\..\\data\\fires_augmented.csv',index_label='id')
+augmented_data_df.to_csv(base_dir + '\\..\\data\\fires_week_long_1000.csv',index_label='id')
